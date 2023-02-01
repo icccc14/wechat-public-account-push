df1daeb7aa68d9e97ed239b587c0de3abade01db
icccc14 <124177227+icccc14@users.noreply.github.com>
2023-02-01 23:45:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '7x76HryB8Og7TOmYj2KGPXLs0coLD30vSP8OLq8aiEo',
 
   CALLBACK_USERS: [
     {
