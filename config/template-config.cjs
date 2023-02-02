135d9f53085fefa9da72925a432e8f5a3308d4c5
icccc14 <124177227+icccc14@users.noreply.github.com>
2023-02-02 14:11:29 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -52,6 +52,32 @@ const TEMPLATE_CONFIG = [
       失败用户: {{fail_post_ids.DATA}}
     `
   },
+  
+  
+  
+  
+  {
+  id：'0003',
+  title '轩轩日记',
+  desc: `
+      **{{date.DATA}}**
+   地区：{{city.DATA}} 
+   天气：{{weather.DATA}} 
+   气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+   天气提醒：{{notice.DATA}} 
+   日出时间：{{sunrise.DATA}} 
+   日落时间：{{sunset.DATA}} 
+   土味情话：{{earthy_love_words.DATA}} 
+   从2005-06-19到现在已破壳{{love_day.DATA}}天
+    {{birthday1.DATA}} {{birthday2.DATA}} 
+   今日建议：
+   {{proposal.DATA}} {{chp.DATA}} 
+   {{note_en.DATA}} 
+   {{note_ch.DATA}}
+  
+  
+  
+  }，
 ]
 
 module.exports = TEMPLATE_CONFIG
