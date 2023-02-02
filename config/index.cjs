e8a906ec1ecd5d35874464c249d3342dfe3c2c03
icccc14 <124177227+icccc14@users.noreply.github.com>
2023-02-02 14:02:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,7 +20,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyvAI6BMZewXCALOtjBskGxy-akg',
+      id: 'oyvAI6HBlGj2czjlxbByD2dQ14eo',
       
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'vbrp2zywwLz444AHm8OAELsH4YIJsTHWOu5E2WA2dB8',
