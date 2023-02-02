02a4cdf7f61492975c965eda530a6448f590542b
icccc14 <124177227+icccc14@users.noreply.github.com>
2023-02-02 15:19:36 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -24,7 +24,7 @@ const TEMPLATE_CONFIG = [
       
       土味情话：{{earthy_love_words.DATA}}
       
-      从2005-06-19到现在已破壳{{love_day.DATA}}天
+      从出生到现在您已破壳{{love_day.DATA}}天
       
        {{birthday1.DATA}}
        
@@ -32,6 +32,7 @@ const TEMPLATE_CONFIG = [
       今日建议：
         {{proposal.DATA}} {{chp.DATA}} 
         {{note_en.DATA}} 
+        
         {{note_ch.DATA}}
     `
   },
@@ -50,6 +51,43 @@ const TEMPLATE_CONFIG = [
       成功用户: {{success_post_ids.DATA}}
       
       失败用户: {{fail_post_ids.DATA}}
+    `
+  },
+  
+    {
+     id: '0003',
+    title: '牛牛日记',
+    desc: `
+      **{{date.DATA}}**
+       
+      ---
+     
+      城市：{{city.DATA}}
+      
+      天气：{{weather.DATA}}
+      
+      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+      
+      天气提醒：{{notice.DATA}}
+      
+      日出时间：{{sunrise.DATA}} 
+      
+      日落时间：{{sunset.DATA}} 
+       
+      ---
+      
+      土味情话：{{earthy_love_words.DATA}}
+      
+      从出生到现在您已破壳{{love_day.DATA}}天
+      
+       {{birthday1.DATA}}
+       
+      ---
+      今日建议：
+        {{proposal.DATA}} {{chp.DATA}} 
+        {{note_en.DATA}} 
+        
+        {{note_ch.DATA}}
     `
   },
  
