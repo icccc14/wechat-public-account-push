c5085cd183e2e986d591785fd5d2da498895b20b
icccc14 <124177227+icccc14@users.noreply.github.com>
2023-02-02 14:13:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,6 +6,8 @@
  */
 const USER_CONFIG = {
 
+  USE_PASSAGE: 'push-plus',
+  
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wxe47a62323e698811',
 
@@ -20,10 +22,10 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyvAI6HBlGj2czjlxbByD2dQ14eo',
+      id: '1ee5af64855a4dba835c6186511605a9',
       
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'vbrp2zywwLz444AHm8OAELsH4YIJsTHWOu5E2WA2dB8',
+      useTemplateId: '0003',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-19',
       festivals: [
