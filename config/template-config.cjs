20de091362b33c2377fa1d142791e127af2eed8f
icccc14 <124177227+icccc14@users.noreply.github.com>
2023-02-02 14:23:35 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -5,35 +5,19 @@ const TEMPLATE_CONFIG = [
     title: '亲爱的, 早上好',
     desc: `
       **{{date.DATA}}**
-      
-      下个休息日：{{holidaytts.DATA}}
-      
-      ---
-      
-      城市：{{city.DATA}}
-      
-      天气：{{weather.DATA}}
-      
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
-      
-      风向: {{wind_direction.DATA}}
-      
-      风级: {{wind_scale.DATA}}
-      
-      {{comprehensive_horoscope.DATA}}
-      
-      ---
-      
-      今天是我们相识的第{{love_day.DATA}}天
-      
-      {{birthday_message.DATA}}
-      
-      ---
-      
-      {{moment_copyrighting.DATA}}
-      
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
-    `
+        地区：{{city.DATA}} 
+        天气：{{weather.DATA}} 
+        气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+        天气提醒：{{notice.DATA}} 
+        日出时间：{{sunrise.DATA}} 
+        日落时间：{{sunset.DATA}} 
+        土味情话：{{earthy_love_words.DATA}} 
+        从2005-06-19到现在已破壳{{love_day.DATA}}天
+         {{birthday1.DATA}} {{birthday2.DATA}} 
+        今日建议：
+        {{proposal.DATA}} {{chp.DATA}} 
+        {{note_en.DATA}} 
+        {{note_ch.DATA}}
   },
   {
     id: '0002',
